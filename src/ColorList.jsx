d5e8e27679474e800e895b6f5df68dd79cd2bec1
apratik552 <apratik552@gmail.com>
2024-10-11 18:56:54 +0530

    first commit

---
+++ src/ColorList.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import { nanoid } from 'nanoid'
+import Color from './Color'
+
+const ColorList = ({ colors }) => {
+    return (
+        <section className="colors">
+            {
+                colors.map((color, index) => {
+                    return <Color key={nanoid()} color={color} index={index} />
+                })
+            }
+        </section>
+    )
+}
+
+export default ColorList
